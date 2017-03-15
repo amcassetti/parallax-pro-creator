f4e8e6ef9f2bf65e1c57c134b681be00779f753d
Andrew Cassetti <andrew@Andrews-MacBook-Air.local>
2017-03-14 20:08:10 -0400

    disk

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+  res.render('index.html');	
+});
+
+var server = app.listen(process.env.PORT || 8081, function(){
+    var port = server.address().port;
+    console.log('App listening on port '+port);
+});
+
+module.exports = app;
